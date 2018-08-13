70db49a841a7580bcd207e6d5a9ff5a183e9bcd0
yeseong0201 <ys020121@naver.com>
2018-08-14 03:26:48 +0900

    first commit

---
+++ app/src/main/java/com/example/ys020/bang_hak_project/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ys020.bang_hak_project;
+
+import android.app.Activity;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
